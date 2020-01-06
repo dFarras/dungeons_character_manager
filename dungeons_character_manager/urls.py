4893a8a0b0d7ad4ee3652b65b84407a6b6a87780
Carlospq <Carlospq@users.noreply.github.com>
2020-01-06 02:05:07 +0100

    Traits: Initial commit

--- dungeons_character_manager/urls.py
+++ dungeons_character_manager/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('race.urls')),
+    path('', include('traits.urls')),
     path('admin/', admin.site.urls),
 ]
---
+++ traits/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # ex: /traits/
+    #path('traits/', views.race, name='race'),
+]
